6b0cb4cc8ddaa476fa9be682c0e4500dfc15a9f1
Atli Snær <atlisa20@hi.is>
2021-06-22 16:20:12 +0000

    Output path changed

--- tagset_revisions/scripts/get_unique_tags.sh
+++ tagset_revisions/scripts/get_unique_tags.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk -F '\t' '{print $2}' ../corpus/sosialurin-corpus_fixes.wip.txt | sort | uniq | sed '/^$/d' |cat > ../sosialurin-unique_tags.txt
+awk -F '\t' '{print $2}' ../corpus/sosialurin-corpus_fixes.wip.txt | sort | uniq | sed '/^$/d' |cat > ../corpus/sosialurin-unique_tags.txt
